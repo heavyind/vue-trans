b0904317bd04f402d2484d00468b6e9baa4d74dd
Chase Ries <hello@chaseries.com>
2019-12-12 19:53:43 -0500

    Add configuration for Babel, minification of UMD
    - Allow package to work in deficient browsers, incl. Safari, IE 11, Edge

--- build/config.js
+++ build/config.js
@@ -1,11 +1,25 @@
 const path = require("path");
+const babel = require("rollup-plugin-babel");
+const resolve = require("@rollup/plugin-node-resolve");
+const commonJs = require("rollup-plugin-commonjs");
+const minify = require("rollup-plugin-babel-minify");
+
 
 const makePath = (f) => {
   return path.join("./dist/", `vue-trans.${f}.js`);
 };
 
+const basePlugins = [
+  babel({
+    exclude: "node_modules/**"
+  }),
+  resolve(),
+  commonJs()
+];
+
 const base = {
-  input: "./index.js"
+  input: "./index.js",
+  plugins: basePlugins
 };
 
 const cjs = {
@@ -20,7 +34,11 @@ const umd = {
     file: makePath("umd"),
     format: "umd",
     name: "VueTrans"
-  }
+  },
+  plugins: [
+    ...basePlugins,
+    minify()
+  ]
 };
 
 const esm = {
